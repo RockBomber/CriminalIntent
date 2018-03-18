defb7f24ae31ab6f00b35082626b1979ad20d4f5
V.Kuznetsov <v.kuznetsov@voskhod.ru>
2018-03-18 19:41:14 +0300

    Improve deleteCrime method for work with SQLite

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -39,7 +39,8 @@ public class CrimeLab {
     }
 
     public void deleteCrime(Crime c) {
-        mCrimes.remove(c);
+        String uuidString = c.getId().toString();
+        mDatabase.delete(CrimeTable.NAME, CrimeTable.Cols.UUID + " = ?", new String[]{uuidString});
     }
 
     public List<Crime> getCrimes() {
